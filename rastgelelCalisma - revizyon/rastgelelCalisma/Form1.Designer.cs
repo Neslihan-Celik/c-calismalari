0b703b1077a7cc80bd9f2fc395a3fbcce93d4a98
Neslihan-Celik <56414227+Neslihan-Celik@users.noreply.github.com>
2020-12-08 19:13:50 +0300

    Rastgele kişiler oluşturan dosya yüklendi

---
+++ rastgelelCalisma - revizyon/rastgelelCalisma/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace rastgelelCalisma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_yazi = new System.Windows.Forms.Label();
+            this.txt_yazi2 = new System.Windows.Forms.Label();
+            this.txt_yazi3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_yazi
+            // 
+            this.txt_yazi.BackColor = System.Drawing.Color.White;
+            this.txt_yazi.Location = new System.Drawing.Point(102, 54);
+            this.txt_yazi.Name = "txt_yazi";
+            this.txt_yazi.Size = new System.Drawing.Size(230, 173);
+            this.txt_yazi.TabIndex = 0;
+            this.txt_yazi.Text = "label1";
+            // 
+            // txt_yazi2
+            // 
+            this.txt_yazi2.BackColor = System.Drawing.Color.White;
+            this.txt_yazi2.Location = new System.Drawing.Point(102, 288);
+            this.txt_yazi2.Name = "txt_yazi2";
+            this.txt_yazi2.Size = new System.Drawing.Size(230, 164);
+            this.txt_yazi2.TabIndex = 1;
+            this.txt_yazi2.Text = "yazı iki";
+            // 
+            // txt_yazi3
+            // 
+            this.txt_yazi3.BackColor = System.Drawing.Color.White;
+            this.txt_yazi3.Location = new System.Drawing.Point(388, 9);
+            this.txt_yazi3.Name = "txt_yazi3";
+            this.txt_yazi3.Size = new System.Drawing.Size(316, 632);
+            this.txt_yazi3.TabIndex = 2;
+            this.txt_yazi3.Text = "random çoklu kişiler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 685);
+            this.Controls.Add(this.txt_yazi3);
+            this.Controls.Add(this.txt_yazi2);
+            this.Controls.Add(this.txt_yazi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_yazi;
+        private System.Windows.Forms.Label txt_yazi2;
+        private System.Windows.Forms.Label txt_yazi3;
+    }
+}
+
---
+++ rastgelelCalisma - revizyon/rastgelelCalisma/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastgelelCalisma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        public class insan
+        {
+            public String ad = "";
+            public String soyAd = "";
+            public String yas = "";
+            public String memleket = " ";
+
+            public string[] isimler = { "ahmet", "mehmet", "ayşe","fatma","cemile","gül","selma" ,"azim","ela","elif","ebru","sedat"};
+            public string[] SoyIsimler = { "çat", "gür", "taş","çelik","coşkun","demir","sarı","adil","salim"};
+            public string[] sehirler = { "istanbul","ankara","şanlıurfa","edirne","bursa","elazığ","giresun","samsun","tokat","ardahan","diyarbakır","kars"};
+            public Random rnd = new Random();
+            
+            public string adGetir()
+            {
+                return this.ad;
+            }
+            public string soyAdGetir()
+            {
+                return this.soyAd;
+            }
+            public string yasGetir()
+            {
+                return this.yas;
+            }
+            public string memleketGetir()
+            {
+                return this.memleket;
+            }
+            public void BilgiGetir()
+            {
+                Console.WriteLine("\nKişi bilgisi \nAd : "+this.adGetir()+
+                                  "\nSoyad : "+this.soyAdGetir()+
+                                  "\nYaş :"+this.yasGetir()+
+                                  "\nMemleket :"+this.memleketGetir());
+            }
+            public void bilgiGir(string ad, string soyAd, string yas,string memleket)
+            {
+                this.ad = ad;
+                this.soyAd = soyAd;
+                this.yas = yas;
+                this.memleket = memleket;
+
+            }
+            public void  randomKisi()
+            {
+                int dgr1 = rnd.Next(isimler.Length);
+                int dgr2 = rnd.Next(SoyIsimler.Length);
+                int dgr3 = rnd.Next(0,90);
+                int dgr4 = rnd.Next(sehirler.Length);
+                this.ad = this.isimler[dgr1];
+                this.soyAd = this.SoyIsimler[dgr2];
+                this.yas = dgr3.ToString();
+                this.memleket = this.sehirler[dgr4];
+
+                Console.WriteLine(String.Join(" ", dgr1, dgr2, dgr3, dgr4)); 
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            insan a = new insan();
+            
+            
+           a.bilgiGir("ali", "veli", "25","gümüşhane");
+           txt_yazi.Text=("Bu kişi bilgisini biz atadık " +
+                        "\n\nKişi bilgisi : \nAd : " + a.adGetir()+
+                        "\nSoyad : "+a.soyAdGetir() +
+                        "\nYaş:"+a.yasGetir()+
+                        "\nMemleket : "+a.memleketGetir());
+
+            a.BilgiGetir();
+            a.randomKisi();
+            txt_yazi2.Text = ("Bu kişi bilgisi tamamen rastgele şekilde üretilmiştir " +
+                            "\n\nKişi bilgisi : \nAd : " + a.adGetir() +
+                            "\nSoyad : " + a.soyAdGetir() + 
+                            "\nYaş:" + a.yasGetir() + 
+                            "\nMemleket : " + a.memleketGetir());
+            
+           for(int i=0;i<5;i++)
+            {
+                    a.randomKisi();
+                     txt_yazi3.Text += ("\n\n"+ (i+1) + ".kişi bilgisi : \nAd : " + a.adGetir() + 
+                                        "\nSoyad : " + a.soyAdGetir() + 
+                                        "\nYaş:" + a.yasGetir() + 
+                                        "\nMemleket : " + a.memleketGetir());
+                    a.BilgiGetir();
+           }
+
+        }
+    }
+}
